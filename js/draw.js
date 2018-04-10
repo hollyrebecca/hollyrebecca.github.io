c0701ef2e0437575f5d5f7e4b34e302477dfdcf4
hollyrebecca <hollyrhendry@gmail.com>
2018-04-10 12:15:02 +0100

    initial commit

---
+++ js/draw.js
@@ -0,0 +1,340 @@
+
+var context;
+
+// each are lists of line elements formatted as {sx: , sy: , ex: , ey: }
+var danger = [];  // red lines
+var platform = [];  // black lines
+var bounce = [];  //blue lines
+
+var black = '#000000';
+var red = '#F00000';
+var blue = '#001eff';
+var white = "#FFFFFF";
+var colour = black;
+
+// TODO: create play button which initialises the game phase
+function play() {
+	var dict = {
+                "danger" : danger,
+                "platform" : platform,
+                "bounce" : bounce
+                }
+
+    /*var dictstring = JSON.stringify(dict);
+    var fs = require('JSON/temp.json');
+    fs.writeFile("temp.json", dictstring);*/
+
+    sessionStorage.drawContent = JSON.stringify(dict);
+
+    window.location.replace("game.html");
+}
+
+
+// =============================================================================
+// Draw state
+// =============================================================================
+
+
+function red_click() {
+	context.strokeStyle = red;
+	colour = red;
+}
+
+function blue_click() {
+	context.strokeStyle = blue;
+	colour = blue;
+}
+
+function black_click() {
+	context.strokeStyle = black;
+	colour = black;
+}
+
+// TODO: Edit to return true if click on line passed
+function intersection(x1, y1, x2, y2, x3, y3, x4, y4){
+	var ia, ib;
+	var denominator = (y4-y3)*(x2-x1) - (x4-x3)*(y2-y1);
+	if (denominator == 0) {
+		return null;
+	}
+	ia = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3))/denominator;
+	ib = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3))/denominator;
+	return {
+		x: x1 + ia*(x2-x1),
+		y: y1 + ia*(y2-y1),
+		seg1: ia >= 0 && ia <= 1,
+		seg2: ib >= 0 && ib <= 1
+	};
+}
+
+function eraseLines(contexto, canvas, x1, y1, x2, y2) {
+    for (var i = 0; i < danger.length; i++){
+       if (intersection(danger[i][0], danger[i][1], danger[i][2], danger[i][3], x1, y1, x2, y2)){
+            //context.globalCompositeOperation = "destination-out";
+            context.lineWidth = danger[i][4];
+            context.strokeStyle = white;
+            context.beginPath();
+            context.moveTo(danger[i][0], danger[i][1]);
+            context.lineTo(danger[i][2], danger[i][3]);
+            context.stroke();
+            
+            danger.splice(i, 1);
+
+            console.log("Remove line");
+        }
+    }
+    for (var i = 0; i < platform.length; i++){
+        if (intersection(platform[i][0], platform[i][1], platform[i][2], platform[i][3], x1, y1, x2, y2)){
+            //context.globalCompositeOperation = "destination-out";
+            context.lineWidth = platform[i][4];
+            context.strokeStyle = white;
+            context.beginPath();
+            context.moveTo(platform[i][0], platform[i][1]);
+            context.lineTo(platform[i][2], platform[i][3]);
+            context.stroke();
+            
+            platform.splice(i, 1);
+
+            console.log("Remove line");
+        }
+    }
+    for (var i = 0; i < bounce.length; i++){
+        if (intersection(bounce[i][0], bounce[i][1], bounce[i][2], bounce[i][3], x1, y1, x2, y2)){
+            //context.globalCompositeOperation = "destination-out";
+            context.lineWidth = bounce[i][4];
+            context.strokeStyle = white;
+            context.beginPath();
+            context.moveTo(bounce[i][0], bounce[i][1]);
+            context.lineTo(bounce[i][2], bounce[i][3]);
+            context.stroke();
+            
+            bounce.splice(i, 1);
+
+            console.log("Remove line");
+        }
+    }
+    contexto.drawImage(canvas, 0, 0);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.globalCompositeOperation = "source-over";
+}
+
+
+// TODO: check if this is the right syntax
+function draw() {
+	var contexto, canvas, canvaso;
+	var tool;
+	var tool_default = 'pen';
+	
+	function init() {
+		canvaso = document.getElementById('drawingCanvas');
+		if (!canvaso) {
+			alert('Error! THe canvas element was not found!');
+			return;
+		}
+
+		if (!canvaso.getContext) {
+			alert('Error! No canvas.getContext!');
+			return;
+		}
+
+		contexto = canvaso.getContext('2d');
+		if (!contexto) {
+			alert('Error! Failed to getContext!');
+			return;
+		}
+
+		var container = canvaso.parentNode;
+		canvas = document.createElement('canvas');
+		if (!canvas) {
+			alert('Error! Cannot create a new canvas element!');
+			return;
+		}
+
+		canvas.id = "tempCanvas";
+		canvas.width = canvaso.width;
+		canvas.height = canvaso.height;
+		container.appendChild(canvas);
+		context = canvas.getContext('2d');
+		context.strokeStyle = "#000000";   // default to black for platforms
+		context.lineWidth = 1.0;
+
+		//context.fillStyle = "#424242";
+		//context.fillRect(0,0,897,532);
+		
+
+		var tool_select = document.getElementById('selector');
+		if (!tool_select) {
+			alert('Error! Failed to get the select element!');
+			return;
+		}
+
+		tool_select.addEventListener('change', ev_tool_change, false);
+
+		if (tools[tool_default]) {
+			tool = new tools[tool_default]();
+			tool_select.value = tool_default;
+		}
+
+		canvas.addEventListener('mousedown', ev_canvas, false);
+		canvas.addEventListener('mousemove', ev_canvas, false);
+		canvas.addEventListener('mouseup', ev_canvas, false);
+	}
+
+	function ev_canvas (ev) {
+		if (ev.layerX || ev.layerX == 0) {
+			ev._x = ev.layerX;
+			ev._y = ev.layerY;
+		} else if (ev.offsetX || ev.offsetX == 0) {
+			ev._x = ev.offsetX;
+			ev._y = ev.offsetY;
+		}
+
+		var func = tool[ev.type];
+		if (func) {
+			func(ev);
+		}
+	} 
+	
+    function ev_tool_change (ev) { 
+	    if (tools[this.value]) { 
+	   		tool = new tools[this.value](); 
+	    } 
+    }
+
+	function img_update() {
+		contexto.drawImage(canvas, 0, 0);
+		context.clearRect(0, 0, canvas.width, canvas.height);
+	}
+
+	var tools = {};
+	var lineList = [];
+
+	tools.pen = function() {
+		var tool = this;
+		var startx;
+		var starty;
+		this.started = false;
+		this.mousedown = function (ev) {
+			context.beginPath();
+			context.moveTo(ev._x, ev._y);
+			startx = ev._x;
+			starty = ev._y;
+			tool.started = true;
+		};
+		this.mousemove = function (ev) {
+			if (tool.started) {
+				context.lineTo(ev._x, ev._y);
+				context.stroke();
+				subline = {sx: startx, sy: starty, ex: ev._x, ey: ev._y, lw: context.lineWidth};
+				lineList.push(subline);
+				startx = ev._x;
+				starty = ev._y;
+			}
+		};
+		this.mouseup = function (ev) {
+			if (tool.started) {
+				tool.mousemove(ev);
+				tool.started = false;
+				img_update();
+
+				switch (colour) {
+					case red: 
+                        danger = danger.concat(lineList);
+                        break
+                    case black:
+                        platform = platform.concat(lineList);
+                        break
+                    case blue:
+                        bounce = bounce.concat(lineList);
+						break
+					default:
+						throw Error("Unexpected colour detected");
+				}
+			}
+		};
+	};
+
+	tools.line = function () {
+		var tool = this;
+		this.started = false;
+		this.mousedown = function (ev) {
+			tool.started = true;
+			tool.x0 = ev._x;
+			tool.y0 = ev._y;
+		}
+		this.mousemove = function (ev) {
+			if (!tool.started) {
+				return;
+			}
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.beginPath();
+			context.moveTo(tool.x0, tool.y0);
+			context.lineTo(ev._x, ev._y);
+			context.stroke();
+			context.closePath();
+		};
+		this.mouseup = function (ev) {
+			if (tool.started) {
+				tool.mousemove(ev);
+				tool.started = false;
+				img_update();
+
+                subline = {sx: tool.x0, sy: tool.y0, ex: ev._x, ey: ev._y, lw: context.lineWidth};
+
+				switch (colour) {
+					case red: 
+						danger = danger.concat(subline);
+						break
+					case black:
+						platform = platform.concat(subline);
+						break
+					case blue:
+						bounce = bounce.concat(subline);
+						break
+					default:
+						throw Error("Unexpected colour detected");
+				}
+			}
+		};
+	};
+
+    tools.eraser = function() {
+        console.log("Eraser");
+        var tool = this;
+        var startx;
+        var starty;
+        this.started = false;
+
+        this.mousedown = function (ev) {
+            startx = ev._x;
+            starty = ev._y;
+            tool.started = true;
+        };
+        this.mousemove = function (ev) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.strokeStyle = white;
+            context.beginPath();
+            context.moveTo(startx, starty);
+            context.lineTo(ev._x, ev._y);
+            context.stroke();
+            context.closePath();
+        };
+        this.mouseup = function (ev) {
+            if (tool.started) {
+                tool.mousemove(ev);
+                tool.started = false;
+                eraseLines(contexto, canvas, startx, starty, ev._x, ev._y);
+                img_update();
+            }
+        };
+    }
+
+init();
+
+}
+
+
+
+window.onload = function () {
+	draw();
+}
---
+++ js/game.js
@@ -0,0 +1,529 @@
+// each are lists of line elements formatted as {sx: , sy: , ex: , ey: }
+var danger = [];  // red lines
+var platform = [];  // black lines
+var bounce = [];  //blue lines
+
+var black = 0x000000;
+var red = 0xF00000;
+var blue = 0x001eff;
+var colour = black;
+
+// TODO: create play button which initialises the game phase
+function playStart() {
+    console.log("Play function");
+    var obj = JSON.parse(sessionStorage.drawContent);
+    danger = obj.danger;
+    platform = obj.platform;
+    bounce = obj.bounce;
+    console.log(obj);
+
+	let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+    game.state.add('play', PlayState);
+    game.state.add('loading', LoadingState);
+    game.state.start('loading');
+}
+
+// =============================================================================
+// Sprites
+// =============================================================================
+
+//
+// Hero
+//
+
+function Hero(game, x, y) {
+    // call Phaser.Sprite constructor
+    Phaser.Sprite.call(this, game, x, y, 'hero');
+
+    // anchor
+    this.anchor.set(0.5, 0.5);
+    this.game.physics.arcade.enable(this);
+    this.enableBody = true;
+    this.alive = true;
+    this.body.collideWorldBounds = true;
+    this.body.immovable = false;
+
+    this.body.maxVelocity.y = 500;
+    // animations
+    this.animations.add('stop', [0]);
+    this.animations.add('run', [1, 2], 8, true); // 8fps looped
+    this.animations.add('jump', [3]);
+    this.animations.add('fall', [4]);
+    this.animations.add('die', [5, 6, 5, 6, 5, 6, 5, 6], 12); // 12fps no loop
+    var dieAnim = this.animations.getAnimation('die');
+
+    dieAnim.onComplete.add(function () {
+        this.kill();
+    }, this);
+
+    // starting animation
+    this.animations.play('stop');
+}
+
+// inherit from Phaser.Sprite
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
+Hero.prototype.move = function (direction) {
+    // guard
+    if (this.isFrozen) { return; }
+
+    const SPEED = 200;
+    this.body.velocity.x = direction * SPEED;
+
+    // update image flipping & animations
+    if (this.body.velocity.x < 0) {
+        this.scale.x = -1;
+    }
+    else if (this.body.velocity.x > 0) {
+        this.scale.x = 1;
+    }
+};
+
+Hero.prototype.jump = function () {
+    const JUMP_SPEED = 400;
+    let canJump = this.body.touching.down && this.alive && !this.isFrozen;
+
+    if (canJump || this.isBoosting) {
+        this.body.velocity.y = -JUMP_SPEED;
+        this.isBoosting = true;
+    }
+
+    return canJump;
+};
+
+Hero.prototype.stopJumpBoost = function () {
+    this.isBoosting = false;
+};
+
+Hero.prototype.bounce = function () {
+    const BOUNCE_SPEED = 200;
+    this.body.velocity.y = -BOUNCE_SPEED;
+};
+
+Hero.prototype.update = function () {
+    // update sprite animation, if it needs changing
+    let animationName = this._getAnimationName();
+    if (this.animations.name !== animationName) {
+        this.animations.play(animationName);
+    }
+};
+
+Hero.prototype.freeze = function () {
+    this.body.enable = false;
+    this.isFrozen = true;
+};
+
+Hero.prototype.die = function () {
+    this.alive = false;
+    this.body.enable = false;
+    this.animations.play('die');
+};
+
+// returns the animation name that should be playing depending on
+// current circumstances
+Hero.prototype._getAnimationName = function () {
+    let name = 'stop'; // default animation
+
+    // dying
+    if (!this.alive) {
+        name = 'die';
+    }
+    // frozen & not dying
+    else if (this.isFrozen) {
+        name = 'stop';
+    }
+    // jumping
+    else if (this.body.velocity.y < 0) {
+        name = 'jump';
+    }
+    // falling
+    else if (this.body.velocity.y >= 0 && !this.body.touching.down) {
+        name = 'fall';
+    }
+    else if (this.body.velocity.x !== 0 && this.body.touching.down) {
+        name = 'run';
+    }
+
+    return name;
+};
+
+// =============================================================================
+// Loading state
+// =============================================================================
+
+LoadingState = {};
+
+LoadingState.init = function () {
+    // keep crispy-looking pixels
+    this.game.renderer.renderSession.roundPixels = true;
+};
+
+LoadingState.preload = function () {
+    this.game.load.image('background', 'images/background.png');
+
+    this.game.load.spritesheet('hero', 'images/hero.png', 36, 42);
+};
+
+LoadingState.create = function () {
+    this.game.state.start('play', true, false);
+};
+
+
+// =============================================================================
+// Play state
+// =============================================================================
+
+PlayState = {};
+var world;
+
+PlayState.init = function () {
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+};
+
+PlayState.create = function () {
+    this.game.time.desiredFps = 60;
+
+    // create level entities and decoration
+    this.game.add.image(0, 0, 'background');
+    
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.world.enableBody = true;
+
+    // TODO: Create level using drawn lines in danger, platform and bounce
+    this._createLevel();
+
+    console.log(this.platforms);
+};
+
+PlayState.update = function () {
+    this._handleCollisions();
+    this._handleInput();
+
+    if (this.hero.body.onFloor()) {
+        this._onHeroVsBound();
+    }
+
+    //this.game.debug.body(this.hero);
+
+    // TODO: has escaped???
+};
+
+PlayState.shutdown = function () {
+    this.hero.animations.destroy();
+};
+
+// TODO: write game phase using phaser. use var line = new Phaser.Line(sx, sy, ex, ey) for each line in danger, platform and bounce.
+// use var graphics = game.add.graphics(line.start.x, line.start.y)   graphics.lineStyle(width, colour, alpha) for each line
+// graphics.drawShape(line)				creates the shape on the canvas
+// graphics.beginFill(colour, alpha)	applies settings to everything drawn after this point
+// graphics.endFill()					stops the settings applied in begin fill 
+
+var ENEMY_GROUP = 0x01;
+var PLATFORM_GROUP = 0x02;
+var BOUNCE_GROUP = 0x03;
+var PLAYER_GROUP = 0x04;
+const HeroX = 300;
+const HeroY = 200;
+
+function calcAngle(x, y, x1, y1) {
+    return Math.atan2(y1 - y, x1 - x);
+};
+
+function calcLength(x, y, x1, y1) {
+    var xs = x1 - x,
+        ys = y1 - y;
+
+    xs *= xs;
+    ys *= ys;
+
+    return Math.sqrt(xs + ys);
+};
+
+/*
+  padding:14px 0 75px 14px; 
+  width:897px; 
+  height:532px; 
+*/
+
+PlayState._addWorldBounds = function() {
+    var boundB = this.game.add.sprite(0, 0);
+    var boundT = this.game.add.sprite(0, 0);
+    var boundL = this.game.add.sprite(0, 0);
+    var boundR = this.game.add.sprite(0, 0);
+    boundB.body.setSize(this.game.width, 2);
+    boundT.body.setSize(this.game.width, 2);
+    boundL.body.setSize(2, this.game.height);
+    boundR.body.setSize(2, this.game.height);
+
+    boundB.alignIn(World.bounds, Phaser.BOTTOM_LEFT, 0, 0);
+    boundT.alignIn(World.bounds, Phaser.TOP_LEFT, 0, 0);
+    boundL.alignIn(World.bounds, Phaser.TOP_LEFT, 0, 0);
+    boundR.alignIn(World.bounds, Phaser.TOP_RIGHT, 0, 0);
+
+    this.bounds.add(boundB);
+    this.bounds.add(boundT);
+    this.bounds.add(boundL);
+    this.bounds.add(boundR);
+
+    this.bounds.allowGravity = false;
+    this.bounds.immovable = true;
+}
+
+PlayState._addPlatforms = function(lines) {
+    if (lines.length == 0){
+        return;
+    }
+    for (i = 0; i < lines.length; i++) {
+        var platformGr = this.game.make.graphics();
+        platformGr.clear();
+        platformGr.lineStyle(lines[i].lw, black, 1);
+        platformGr.moveTo(lines[i].sx, lines[i].sy);
+        platformGr.lineTo(lines[i].ex, lines[i].ey);
+        platformGr.boundsPadding = 0;
+
+        var texture = platformGr.generateTexture();
+
+        platformGr.destroy();
+        
+        var startX = lines[i].sx;
+        var startY = lines[i].sy;
+
+        if (lines[i].sy > lines[i].ey) {
+            startY = lines[i].ey;
+        }
+
+        if (lines[i].sx > lines[i].ex) {
+            startX = lines[i].ex;
+        }
+
+        var shapeSprite = this.game.add.sprite(startX, startY, texture);
+
+        this.game.physics.enable(shapeSprite, Phaser.Physics.ARCADE);
+
+        shapeSprite.enableBody = true;
+        shapeSprite.body.mass = 2000;
+        shapeSprite.body.immovable = true;
+        shapeSprite.body.allowGravity = false;
+
+        //this.game.debug.body(shapeSprite);
+
+        this.platforms.add(shapeSprite);
+    }
+};
+
+PlayState._addEnemies = function(lines) {
+    if (lines.length == 0){
+        return;
+    }
+    for (i = 0; i < lines.length; i++) {
+        var startX = lines[i].sx;
+        var startY = lines[i].sy;
+
+        var platformGr = this.game.make.graphics();
+        platformGr.clear();
+        platformGr.lineStyle(lines[i].lw, red, 1);
+        platformGr.moveTo(startX, startY);
+        platformGr.lineTo(lines[i].ex, lines[i].ey);
+        platformGr.boundsPadding = 0;
+
+        var texture = platformGr.generateTexture();
+
+        platformGr.destroy();
+
+        var shapeSprite = this.game.add.sprite(startX, startY, texture);
+        //shapeSprite.addChild(platformGr);
+
+        /*// create a group for all the player's hitboxes     
+        hitboxes = this.game.add.group();     
+        // give all the hitboxes a physics body (I'm using arcade physics btw)     
+        hitboxes.enableBody = true;   
+        var offsetX = 0;
+        var offsetY = 0;              
+        // set the size of the hitbox, and its position relative to the player       
+        for(var i = 0; i < 8; i++){          
+        // if we find the hitbox with the "name" specified          
+                // create a "hitbox" (really just an empty sprite with a physics body)     
+            var hitbox = hitboxes.create(0,0,null);  
+            hitbox.body.setSize(shapeSprite.width/8, shapeSprite.height / 8, startX + offsetX, startY + offsetY);  
+            offsetX += shapeSprite.width/8;
+            offsetY += shapeSprite.height / 8;
+            this.game.debug.body(hitbox);
+        }
+
+        //shapeSprite.addChild(hitboxes);*/
+
+        this.game.physics.enable(shapeSprite, Phaser.Physics.ARCADE);
+
+        shapeSprite.body.collideWorldBounds = true;
+        shapeSprite.body.immovable = true;
+        shapeSprite.body.allowGravity = false;
+        shapeSprite.enableBody = true;
+        shapeSprite.body.mass = 100;
+
+        //this.game.debug.body(shapeSprite);
+
+        this.enemies.add(shapeSprite);
+    }
+};
+
+PlayState._addBounces = function(lines) {
+    if (lines.length == 0){
+        return;
+    }
+    for (i = 0; i < lines.length; i++) {
+        var platformGr = this.game.make.graphics();
+        platformGr.clear();
+        platformGr.lineStyle(lines[i].lw, blue, 1);
+        platformGr.moveTo(lines[i].sx, lines[i].sy);
+        platformGr.lineTo(lines[i].ex, lines[i].ey);
+        platformGr.boundsPadding = 0;
+
+        var texture = platformGr.generateTexture();
+
+        platformGr.destroy();
+
+        var shapeSprite = this.game.add.sprite(lines[i].sx, lines[i].sy, texture);
+
+        this.game.physics.enable(shapeSprite, Phaser.Physics.ARCADE);
+
+        shapeSprite.enableBody = true;
+        shapeSprite.body.mass = 100;
+        shapeSprite.body.immovable = true;
+        shapeSprite.body.allowGravity = false;
+        shapeSprite.body.bounce.y = 0.5;
+
+        //this.game.debug.body(shapeSprite);
+
+        this.bounces.add(shapeSprite);
+    }
+};
+
+PlayState._createLevel = function() {
+	this.enemies = this.game.add.group();
+	this.platforms = this.game.add.group();
+	this.bounces = this.game.add.group();
+    this.bounds = this.game.add.group();
+
+    this.enemies.enableBody = true;
+    this.platforms.enableBody = true;
+    this.bounces.enableBody = true;
+    this.bounds.enableBody = true;
+
+	var line = new Phaser.Line();
+	
+
+    console.log("adding platforms");
+	this._addPlatforms(platform);
+    console.log("adding enemies");
+	this._addEnemies(danger);
+    console.log(danger);
+    console.log("adding bounces");
+	this._addBounces(bounce);
+    console.log(bounce);
+
+    //this._addWorldBounds();
+
+    this.hero = new Hero(this.game, HeroX, HeroY);
+    this.game.add.existing(this.hero);
+
+    const GRAVITY = 980;  // Earth gravity
+    this.game.physics.arcade.gravity.y = GRAVITY;
+};
+
+// TODO: Edit to make enemies anything in red.
+PlayState._handleCollisions = function () {
+    //this.game.physics.arcade.collide(this.spiders, this.platforms);
+    //this.game.physics.arcade.collide(this.spiders, this.enemyWalls);
+    this.game.physics.arcade.collide(this.hero, this.platforms);
+
+    this.game.physics.arcade.collide(this.hero, this.bounces, this._onHeroVsBounce, null, this);
+
+    this.game.physics.arcade.collide(this.hero, this.bounds, this._onHeroVsBound, null, this);
+
+    // hero vs coins (pick up)
+    //this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin,
+        //null, this);
+    // hero vs key (pick up)
+    //this.game.physics.arcade.overlap(this.hero, this.key, this._onHeroVsKey,
+        //null, this);
+    // hero vs door (end level)
+    //this.game.physics.arcade.overlap(this.hero, this.door, this._onHeroVsDoor,
+        // ignore if there is no key or the player is on air
+        //function (hero, door) {
+        //    return this.hasKey && hero.body.touching.down;
+        //}, this);
+    //collision: hero vs enemies (kill or die)
+    this.game.physics.arcade.overlap(this.hero, this.enemies,
+        this._onHeroVsEnemy, null, this);
+};
+
+PlayState._onHeroVsBound = function () {
+    alert('You won the game, congratulations!');
+    this.game.state.restart(true, false);
+}
+
+PlayState._handleInput = function () {
+    if (this.cursors.left.isDown) { // move hero left
+        this.hero.move(-1);
+    }
+    else if (this.cursors.right.isDown) { // move hero right
+        this.hero.move(1);
+    }
+    else { // stop
+        this.hero.move(0);
+    }
+
+    // handle jump
+    const JUMP_HOLD = 200; // ms
+    if (this.cursors.up.isDown && this.hero.body.touching.down) {
+        //let didJump = this.hero.jump();
+        //if (didJump) { this.sfx.jump.play(); }
+        this.hero.jump();
+    }
+};
+
+PlayState._onHeroVsPlatform = function (hero, platform) {
+    // game over -> play dying animation and restart the game
+    console.log("collided with platform");
+};
+
+PlayState._onHeroVsEnemy = function (hero, enemy) {
+    // game over -> play dying animation and restart the game
+    console.log("Enemy Collision");
+    hero.die();
+    hero.events.onKilled.addOnce(function () {
+        this.game.state.restart(true, false);
+    }, this);
+
+    // NOTE: bug in phaser in which it modifies 'touching' when
+    // checking for overlaps. This undoes that change so enemies don't
+    // 'bounce' agains the hero
+    enemy.body.touching = enemy.body.wasTouching;
+};
+
+
+// TODO: Congratulate on level completion
+PlayState._onHeroVsBounce = function (hero, bounce) {
+    console.log("Bounce Collision");
+    hero.body.velocity.y = -800;
+};
+
+PlayState._updateGravity = function (newGrav) {
+    this.game.physics.gravity = newGrav * 100;
+};
+
+
+function newDraw() {
+    window.location.replace("index.html");
+}
+
+function change_gravity() {
+    var newGrav = document.getElementById('Gravity Value').value;
+    PlayState._updateGravity(newGrav);
+}
+
+
+window.onload = function () {
+    playStart();
+}
